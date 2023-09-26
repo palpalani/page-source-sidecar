d292156784484b6910960056735a37a33ae994c9
palPalani <pal@targetbay.com>
2023-09-26 23:48:18 +0530

    :construction: wip

--- lambda/sidecar.js
+++ lambda/sidecar.js
@@ -1,13 +1,46 @@
-// import puppeteer from 'puppeteer'
-// const chromeLambda = require('chrome-aws-lambda');
+import chromium from '@sparticuz/chromium'
+import puppeteer from 'puppeteer-core'
+
+export const handle = async (event, context, callback) => {
+    let result = null;
+    let browser = null;
+
+    try {
+        browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath(),
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+        });
+
+        let page = await browser.newPage();
+
+        await page.goto(event.url || 'https://targetbay.com');
+
+        result = await page.title();
+    } catch (error) {
+        return callback(error);
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
+
+    return callback(null, result);
+};
+
+/*
 import chromeLambda from 'chrome-aws-lambda'
 
 export const handle = async (event) => {
+    let browser = null;
+    let result = ''
+
     try {
-        let result = ''
 
         // Launch a headless chrome browser.
-        const browser = await chromeLambda.puppeteer.launch({
+        browser = await chromeLambda.puppeteer.launch({
             args: chromeLambda.args,
             executablePath: await chromeLambda.executablePath,
             defaultViewport: chromeLambda.defaultViewport,
@@ -17,25 +50,22 @@ export const handle = async (event) => {
 
         const page = await browser.newPage();
 
-    	await page.goto(event.url);
+        await page.goto(event.url);
 
         //result = await page.content();
-    	// OR the faster method that doesn't wait for images to load:
-    	result = await page.content({"waitUntil": "domcontentloaded"});
+        // OR the faster method that doesn't wait for images to load:
+        result = await page.content({"waitUntil": "domcontentloaded"});
 
         await page.close();
-    	await browser.close();
+        await browser.close();
 
     } catch (error) {
         return formatError(error);
-    }
-    /*
-    finally {
+    } finally {
         if (browser !== null) {
           await browser.close();
         }
     }
-    */
 
     return JSON.stringify(result);
 };
@@ -51,6 +81,7 @@ var formatError = function (error) {
         body: error.code + ': ' + error.message,
     };
 };
+*/
 
 /*
 export const handle = async function (event) {
@@ -58,13 +89,13 @@ export const handle = async function (event) {
 
     try {
         const browser = await puppeteer.launch();
-   	const page = await browser.newPage();
-    	await page.goto(event.url);
+          const page = await browser.newPage();
+        await page.goto(event.url);
 
-    	//result = await page.content();
-    	// OR the faster method that doesn't wait for images to load:
-    	result = await page.content({"waitUntil": "domcontentloaded"});
-    	browser.close();
+        //result = await page.content();
+        // OR the faster method that doesn't wait for images to load:
+        result = await page.content({"waitUntil": "domcontentloaded"});
+        browser.close();
     } catch (exception) {
         return JSON.stringify({pageSource: exception.toString()});
     }
