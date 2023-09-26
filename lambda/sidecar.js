c56369ad19b36aa4afb4fee7dfdb1dab2cff2c87
palPalani <pal@targetbay.com>
2023-09-26 21:15:42 +0530

    :construction: wip

--- lambda/sidecar.js
+++ lambda/sidecar.js
@@ -1,5 +1,48 @@
-import puppeteer from 'puppeteer'
+// import puppeteer from 'puppeteer'
+const chromeLambda = require('chrome-aws-lambda');
 
+exports.handler = async (event) => {
+    try {
+        let result = ''
+
+        // Launch a headless chrome browser.
+        const browser = await chromeLambda.puppeteer.launch({
+            args: chromeLambda.args,
+            executablePath: await chromeLambda.executablePath,
+            defaultViewport: chromium.defaultViewport,
+            headless: chromium.headless,
+            ignoreHTTPSErrors: true,
+        });
+
+        const page = await browser.newPage();
+
+    	await page.goto(event.url);
+
+        //result = await page.content();
+    	// OR the faster method that doesn't wait for images to load:
+    	result = await page.content({"waitUntil": "domcontentloaded"});
+    	await browser.close();
+
+    } catch (error) {
+        return formatError(error);
+    }
+
+    return JSON.stringify(result);
+};
+
+var formatError = function (error) {
+    return {
+        statusCode: error.statusCode,
+        headers: {
+            'Content-Type': 'text/plain',
+            'x-amzn-ErrorType': error.code,
+        },
+        isBase64Encoded: false,
+        body: error.code + ': ' + error.message,
+    };
+};
+
+/*
 export const handle = async function (event) {
     let result = ''
 
@@ -18,3 +61,5 @@ export const handle = async function (event) {
 
     return JSON.stringify(result);
 }
+
+*/
